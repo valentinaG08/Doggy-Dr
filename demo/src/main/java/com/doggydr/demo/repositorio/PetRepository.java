acd19aa60a30c4ccc8303e45aaaf6ddc15345c20
nunezpl <nunezpl@javeriana.edu.co>
2024-08-14 21:17:59 -0500

    Mejora estilo ver mascotas y asociacion con ver una mascota

--- demo/src/main/java/com/doggydr/demo/repositorio/PetRepository.java
+++ demo/src/main/java/com/doggydr/demo/repositorio/PetRepository.java
@@ -9,9 +9,9 @@ import com.doggydr.demo.entidad.Pet;
 public class PetRepository {
     private Map<Integer, Pet> data = new HashMap<>();
     public PetRepository(){
-        data.put(1, new Pet("Perry", "French Poodle", 2, "Baño"));
-        data.put(2, new Pet("Lucas", "Labrador", 2, "Baño"));
-        data.put(3, new Pet("Zeus", "Golden Retriever", 2, "Tratamiento"));
+        data.put(1, new Pet(1, "Perry", "French Poodle", 2, "Baño", "https://wowmascota.com/wp-content/uploads/2019/05/pets-753464_640.jpg"));
+        data.put(2, new Pet(2, "Lucas", "Labrador", 2, "Baño", "https://es.mypet.com/wp-content/uploads/sites/23/2021/03/ThinkstockPhotos-590080440.jpg?w=1024"));
+        data.put(3, new Pet(3, "Zeus", "Golden Retriever", 2, "Tratamiento", "https://t2.ea.ltmcdn.com/es/posts/1/6/2/10_curiosidades_del_golden_retriever_21261_orig.jpg"));
     }
     public Pet findById(int id){
         return data.get(id);
