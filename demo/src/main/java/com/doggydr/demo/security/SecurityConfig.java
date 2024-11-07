8b065565bd7708c64c34ef661c01e74678051458
Valentina Garcia <valentinagarciaalfonso@gmail.com>
2024-11-07 02:00:41 -0500

    security

--- demo/src/main/java/com/doggydr/demo/security/SecurityConfig.java
+++ demo/src/main/java/com/doggydr/demo/security/SecurityConfig.java
@@ -4,6 +4,8 @@ package com.doggydr.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,5 +34,12 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    )throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();  
     }
 }
